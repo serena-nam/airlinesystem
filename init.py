187cd1c5f617f4d812101a0264b76e58d8df1e81
branmkim <bran.mkim@gmail.com>
2024-11-21 15:09:16 -0500

    getFlights backend

--- init.py
+++ init.py
@@ -7,122 +7,48 @@ app = Flask(__name__)
 
 #Configure MySQL
 conn = pymysql.connect(host='localhost',
-                       user='root',
-                       password='',
-                       db='blog',
-                       charset='utf8mb4',
-                       cursorclass=pymysql.cursors.DictCursor)
+						user='root',
+						password='',
+						db='project',
+						charset='utf8mb4',
+						cursorclass=pymysql.cursors.DictCursor)
 
 #Define a route to hello function
 @app.route('/')
 def hello():
-	return render_template('index.html')
+	return render_template('home.html')
 
-#Define route for login
-@app.route('/login')
-def login():
-	return render_template('login.html')
 
-#Define route for register
-@app.route('/register')
-def register():
-	return render_template('register.html')
-
-#Authenticates the login
-@app.route('/loginAuth', methods=['GET', 'POST'])
-def loginAuth():
+@app.route('/getFlights', methods=['POST'])
+def getFlights():
 	#grabs information from the forms
-	username = request.form['username']
-	password = request.form['password']
-
+	departure_airport_code = request.form.get('departure_airport_code')
+	arrival_airport_code = request.form.get('arrival_airport_code')
+	departure_date = request.form.get('departure_date')
+ 
 	#cursor used to send queries
 	cursor = conn.cursor()
 	#executes query
-	query = 'SELECT * FROM user WHERE username = %s and password = %s'
-	cursor.execute(query, (username, password))
+	query = '''SELECT * FROM Flight WHERE 
+ 		departure_airport_code = %s and
+   		arrival_airport_code = %s and
+		departure_date_time >= %s
+     '''
+	cursor.execute(query, (departure_airport_code, arrival_airport_code, departure_date))
+	# query = 'SELECT * FROM Flight'
+	# cursor.execute(query)
 	#stores the results in a variable
-	data = cursor.fetchone()
+	data = cursor.fetchall()
 	#use fetchall() if you are expecting more than 1 data row
 	cursor.close()
 	error = None
-	if(data):
-		#creates a session for the the user
-		#session is a built in
-		session['username'] = username
-		return redirect(url_for('home'))
+
+	app.logger.info(data)
+	if data:
+		return render_template("yes.html", flights=data)
 	else:
-		#returns an error message to the html page
-		error = 'Invalid login or username'
-		return render_template('login.html', error=error)
+		return render_template("no.html")
 
-#Authenticates the register
-@app.route('/registerAuth', methods=['GET', 'POST'])
-def registerAuth():
-	#grabs information from the forms
-	username = request.form['username']
-	password = request.form['password']
-
-	#cursor used to send queries
-	cursor = conn.cursor()
-	#executes query
-	query = 'SELECT * FROM user WHERE username = %s'
-	cursor.execute(query, (username))
-	#stores the results in a variable
-	data = cursor.fetchone()
-	#use fetchall() if you are expecting more than 1 data row
-	error = None
-	if(data):
-		#If the previous query returns data, then user exists
-		error = "This user already exists"
-		return render_template('register.html', error = error)
-	else:
-		ins = 'INSERT INTO user VALUES(%s, %s)'
-		cursor.execute(ins, (username, password))
-		conn.commit()
-		cursor.close()
-		return render_template('index.html')
-
-@app.route('/home')
-def home():
-    
-    username = session['username']
-    cursor = conn.cursor();
-    query = 'SELECT ts, blog_post FROM blog WHERE username = %s ORDER BY ts DESC'
-    cursor.execute(query, (username))
-    data1 = cursor.fetchall() 
-    for each in data1:
-        print(each['blog_post'])
-    cursor.close()
-    return render_template('home.html', username=username, posts=data1)
-
-		
-@app.route('/post', methods=['GET', 'POST'])
-def post():
-	username = session['username']
-	cursor = conn.cursor();
-	blog = request.form['blog']
-	query = 'INSERT INTO blog (blog_post, username) VALUES(%s, %s)'
-	cursor.execute(query, (blog, username))
-	conn.commit()
-	cursor.close()
-	return redirect(url_for('home'))
-
-
-@app.route('/search', methods=['GET', 'POST'])
-def post():
-	username = session['username']
-	cursor = conn.cursor();
-	blog = request.form['blog']
-	query = 'INSERT INTO blog (blog_post, username) VALUES(%s, %s)'
-	cursor.execute(query, (blog, username))
-	conn.commit()
-	cursor.close()
-	return redirect(url_for('home'))
-
-@app.route('/logout')
-def logout():
-	session.pop('username')
-	return redirect('/')
 		
 app.secret_key = 'some key that you will never guess'
 #Run the app on localhost port 5000
